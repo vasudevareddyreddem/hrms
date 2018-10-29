9059fdc8f9eb4b23969a7b257f36f9827f4f23a9
vasudevareddyreddem <vasudevareddy549@gmail.com>
2018-10-29 11:27:02 +0530

    adding  db

---
+++ assets/hrmsdb.sql
@@ -0,0 +1,106 @@
+/*
+SQLyog Community v11.52 (64 bit)
+MySQL - 10.1.32-MariaDB : Database - hrms
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`hrms` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `hrms`;
+
+/*Table structure for table `empployee` */
+
+DROP TABLE IF EXISTS `empployee`;
+
+CREATE TABLE `empployee` (
+  `e_id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_id` int(11) DEFAULT NULL,
+  `e_emplouee_id` varchar(250) DEFAULT NULL,
+  `e_join_date` varchar(250) DEFAULT NULL,
+  `e_f_name` varchar(250) DEFAULT NULL,
+  `e_l_name` varchar(250) DEFAULT NULL,
+  `e_login_name` varchar(250) DEFAULT NULL,
+  `e_email_personal` varchar(250) DEFAULT NULL,
+  `e_email_work` varchar(250) DEFAULT NULL,
+  `e_password` varchar(250) DEFAULT NULL,
+  `e_org_password` varchar(250) DEFAULT NULL,
+  `e_mobile_personal` varchar(250) DEFAULT NULL,
+  `e_mobile_work` varchar(250) DEFAULT NULL,
+  `e_designation` varchar(250) DEFAULT NULL,
+  `e_supervisor` varchar(250) DEFAULT NULL,
+  `e_department` varchar(250) DEFAULT NULL,
+  `e_sub_department` varchar(250) DEFAULT NULL,
+  `e_c_adress` varchar(250) DEFAULT NULL,
+  `e_c_city` varchar(250) DEFAULT NULL,
+  `e_c_district` varchar(250) DEFAULT NULL,
+  `e_c_state` varchar(250) DEFAULT NULL,
+  `e_c_country` varchar(250) DEFAULT NULL,
+  `e_p_address` varchar(250) DEFAULT NULL,
+  `e_p_city` varchar(250) DEFAULT NULL,
+  `e_p_district` varchar(250) DEFAULT NULL,
+  `e_p_state` varchar(250) DEFAULT NULL,
+  `e_p_country` varchar(250) DEFAULT NULL,
+  `e_profile_pic` varchar(250) DEFAULT NULL,
+  `e_document` varchar(250) DEFAULT NULL,
+  `e_bank_name` varchar(250) DEFAULT NULL,
+  `e_account_number` varchar(250) DEFAULT NULL,
+  `e_bank_h_name` varchar(250) DEFAULT NULL,
+  `e_bank_ifcs_code` varchar(250) DEFAULT NULL,
+  `e_c_p_name` varchar(250) DEFAULT NULL,
+  `e_c_p_mobile` varchar(250) DEFAULT NULL,
+  `e_c_p_email` varchar(250) DEFAULT NULL,
+  `e_c_p_relationship` varchar(250) DEFAULT NULL,
+  `e_c_p_address` varchar(250) DEFAULT NULL,
+  `status` int(11) DEFAULT '1' COMMENT '0=deactive;1=active;2=delete;',
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`e_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `empployee` */
+
+/*Table structure for table `login_details` */
+
+DROP TABLE IF EXISTS `login_details`;
+
+CREATE TABLE `login_details` (
+  `l_id` int(11) NOT NULL AUTO_INCREMENT,
+  `e_id` int(11) DEFAULT NULL COMMENT 'employee id',
+  `e_login_time` datetime DEFAULT NULL,
+  `e_logout_time` datetime DEFAULT NULL,
+  `l_date` date DEFAULT NULL,
+  `created_by` int(11) DEFAULT NULL,
+  PRIMARY KEY (`l_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `login_details` */
+
+/*Table structure for table `role` */
+
+DROP TABLE IF EXISTS `role`;
+
+CREATE TABLE `role` (
+  `r_id` int(11) NOT NULL AUTO_INCREMENT,
+  `role` varchar(250) DEFAULT NULL,
+  `status` int(11) DEFAULT '1',
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`r_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Data for the table `role` */
+
+insert  into `role`(`r_id`,`role`,`status`,`created_at`) values (1,'Admin',1,'2018-10-29 10:58:13'),(2,'Hr',1,'2018-10-29 10:58:30'),(3,'sales',1,'2018-10-29 10:59:03');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
