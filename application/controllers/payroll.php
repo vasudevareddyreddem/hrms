22b81f3a9527745daf713d8860ab45b7c4c9f575
kasiumamahesh <43772642+kasiumamahesh@users.noreply.github.com>
2018-11-02 15:38:30 +0530

    commit

--- application/controllers/payroll.php
+++ application/controllers/payroll.php
@@ -402,6 +402,38 @@ public function sal_delete($eid){
       
     }
 
+  public function gen_pdf(){
+    ///load mPDF library
+    $this->load->library('Pdf');
+    //load mPDF library
+ 
+ 
+    //now pass the data//
+     // $this->data['title']="MY PDF TITLE 1.";
+     // $this->data['description']="";
+     // $this->data['description']=$this->official_copies;
+     //now pass the data //
+ 
+    
+    $html=$this->load->view('employee/leaves'); //load the pdf_output.php by passing our data and get all data in $html varriable.
+   
+    //this the the PDF filename that user will get to download
+    //$pdfFilePath ="mypdfName-".time()."-download.pdf";
+ $pdfFilePath='payslip.pdf';
+    
+    //actually, you can pass mPDF parameter on this load() function
+    $pdf= $this->pdf->load();
+    //generate the PDF!
+    $pdf->WriteHTML($html,2);
+    //offer it to user via browser download! (The PDF won't be saved on your server HDD)
+    $pdf->Output($pdfFilePath, "D");
+
+
+
+
+
+  }
+
 
 	}
 	?>
