7d25bea33ca27bbdc28254f1d136237644c17318
kasiumamahesh <43772642+kasiumamahesh@users.noreply.github.com>
2018-11-08 18:06:06 +0530

    thu commit

--- application/controllers/Employee.php
+++ application/controllers/Employee.php
@@ -1576,14 +1576,17 @@ public function chat(){
 
     $data['rec_det']=$this->Chat_model->recv_details($rid);
 
-         }else{
+         }
+         else{
          	$rid=$lastdata->sender_id;
           	 $this->session->set_userdata('recv',$rid);
           	$data['rec_det']=$this->Chat_model->recv_details($rid);
-}
+            }
 
 $data['chatdata']=$this->Chat_model->last_chat($eid,$rid);
+$this->Chat_model->read_status_change($eid,$rid);
 }
+
 
          //$data['rec_det']=$this->Chat_model->last_chat_rec_id($eid);
         
@@ -1602,7 +1605,7 @@ $data['chatdata']=$this->Chat_model->last_chat($eid,$rid);
           
           //$this->session->set_userdata('recv',$rid);
       
-          echo'<pre>';print_r($data);exit;
+          //echo'<pre>';print_r($data);exit;
 
  
 	     $this->load->view('employee/sidebar-chat',$data);
--- application/models/chat_model.php
+++ application/models/chat_model.php
@@ -121,7 +121,7 @@ public function employee_info($rid){
 
 
 }
-//update the messages read
+//update the messages unread to read
 public function read_status_change($sid,$rid){
 
   $this->db->update('chat_tab', array('read_status'=>'read'),
@@ -144,6 +144,7 @@ public function getlastrecord($eid){
 $query=$this->db->get();
 return $query->row();
 }
+//receiver details
 public function recv_details($rid){
 
   $this->db->select('*')->from('empployee')->join('role','empployee.role_id=role.r_id')
@@ -151,7 +152,61 @@ public function recv_details($rid){
   $query=$this->db->get();
   return $query->row();
 
+}
+//who are loggedin 
+public function update_login_users(){
+
+   $this->db->select('e_id,e_f_name')->from('empployee')->where('login_status',1);
+   $query=$this->db->get();
+   return $query->result();
 
 
 }
+public function updates_for_users($sid,$rid){
+
+// $this->db->select('receiver_id,count(*) cnt')->from('chat_tab')->where('recevier_id !=',$rid)
+// ->where('sender_id',$sid)->where('read_status','unread')->group_by('recevier_id');
+//    $subquery=$this->db->get_compiled_select();
+//    $this->db->select('e_id, e_f_name,login_status,cnt')->from($subquery)->
+//    join('empployee',)
+//    return $query->result();
+  $sql='select e_id,e_f_name,cnt,login_status from (select sender_id,recevier_id,count(*) cnt from chat_tab where recevier_id ='.$sid.' and sender_id !='.$rid .' and read_status="unread" and notified_msg=0 group by sender_id,recevier_id) chat right join empployee on (empployee.e_id=chat.sender_id)  where e_id !='.$sid;
+ $query = $this->db->query($sql);
+
+// $this->db->select('e_id,e_f_name, cnt,login_status');
+//  $sub= $this->subquery->start_subquery('from');
+// $sub->select('receiver_id,count(*) cnt');
+// $sub->from('chat_tab');
+// $sub->where('recevier_id !=',$rid);
+// $sub->where('sender_id',$sid);
+// $sub->where('read_status','unread');
+// $sub->group_by('recevier_id');
+// $this->subquery->end_subquery('chat');
+// $this->db->join('empployee', 'empployee.e_id=chat.recevier_id');
+// $query=$this->db->get();
+return $query->result();
+
+}
+public function update_msg_count($sid,$rid){
+
+
+  $sql='select sender_id,recevier_id,count(*) cnt from chat_tab where recevier_id ='.$sid.' and sender_id !='.$rid .' and read_status="unread" and notified_msg=0 group by sender_id,recevier_id ';
+ $query = $this->db->query($sql);
+
+
+return $query->result();
+
+}
+
+
+public function notified_change($sid)
+{
+  $this->db->set('notified_msg', 1);
+$this->db->where('recevier_id=', $sid);
+$this->db->update('chat_tab'); 
+
+return 'success';
+
+}
+
 }
