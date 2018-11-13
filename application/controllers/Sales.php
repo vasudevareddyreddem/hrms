f8405f007de2b0ef280e651707cd30f5c40f9c0e
vasudevareddyreddem <vasudevareddy549@gmail.com>
2018-11-13 17:09:29 +0530

    sales  man track  details
    sales  man track  details

--- application/controllers/Sales.php
+++ application/controllers/Sales.php
@@ -18,8 +18,8 @@ class Sales extends In_frontend {
 	   
 	  if($this->session->userdata('hrmsdetails'))
 		{	
-	  $data['sales_list']=$this->Sales_model->get_sales_details_list();
-	 //echo'<pre>';print_r($data);exit;
+		$data['sales_list']=$this->Sales_model->get_sales_details_list();
+			//echo'<pre>';print_r($data);exit;
 	  
 			$this->load->view('sales/salestrack',$data);
 			$this->load->view('html/footer');  
@@ -35,10 +35,12 @@ class Sales extends In_frontend {
 		{	
          $admindetails=$this->session->userdata('hrmsdetails');	
 		 
-		 $data['sale_man_details']=$this->Sales_model->get_sales_man_details();
+		 $emp_id=base64_decode($this->uri->segment(3));
+		 $data['sale_man_details']=$this->Sales_model->get_sales_man_details($emp_id);
 		 //echo'<pre>';print_r($data);exit;
-		 $data['area_location_list']=$this->Sales_model->get_sales_detais_date_wise();
-		// echo'<pre>';print_r($data);exit;
+		 $data['prevoius_date']=$this->Sales_model->get_employee_previous_date_location_details($emp_id,date('Y-m-d', strtotime(' -1 day')));
+		 $data['current_date']=$this->Sales_model->get_employee_current_date_location_details($emp_id,date('Y-m-d'));
+		  //echo'<pre>';print_r($data);exit;
 		 
 	     $this->load->view('sales/track-details',$data);
 	     $this->load->view('html/footer');   
--- application/models/Sales_model.php
+++ application/models/Sales_model.php
@@ -10,27 +10,42 @@ class Sales_model extends CI_Model
 	}
 	
     public function get_sales_details_list(){
-	$this->db->select('area.area,sales_track_details.*,role.role,assign_work.allocated_area,mobile_number,empployee.e_emplouee_id,e_login_name,e_designation,e_join_date')->from('sales_track_details');
-	$this->db->join('empployee', 'empployee.role_id = sales_track_details.sales_emp_id', 'left');
-	$this->db->join('role', 'role.r_id = sales_track_details.sales_emp_id', 'left');
-	$this->db->join('assign_work', 'assign_work.work_employee_id = sales_track_details.sales_emp_id', 'left');
-	 $this->db->join('area', 'area.a_id = assign_work.allocated_area ', 'left');
+	$this->db->select('role.role,area.area,sales_track_details.*,assign_work.work_employee_id,assign_work.mobile_number,empployee.e_emplouee_id,e_login_name,e_designation,e_join_date')->from('sales_track_details');
+	$this->db->join('empployee', 'empployee.e_id = sales_track_details.sales_emp_id', 'left');
+	$this->db->join('assign_work', 'assign_work.w_d_id = sales_track_details.assign_work_id', 'left');
+	$this->db->join('area', 'area.a_id = assign_work.allocated_area', 'left');
+	$this->db->join('role', 'role.r_id = empployee.role_id', 'left');
 	$this->db->group_by('sales_track_details.sales_emp_id');
 	return $this->db->get()->result_array();
 	}
 	
-	public function get_sales_man_details(){
-	$this->db->select('area.area,sales_track_details.*,empployee.*,assign_work.allocated_area')->from('sales_track_details');	
+	public function get_sales_man_details($emp_id){
+	$this->db->select('area.area,sales_track_details.*,empployee.e_id,empployee.e_profile_pic,empployee.e_mobile_personal,empployee.e_email_work,empployee.role_id,empployee.e_emplouee_id,empployee.e_c_adress,empployee.e_c_city,empployee.e_c_district,empployee.e_c_state,empployee.e_c_country,empployee.e_login_name,assign_work.allocated_area')->from('sales_track_details');	
 	$this->db->join('empployee', 'empployee.role_id = sales_track_details.sales_emp_id', 'left');
 	$this->db->join('assign_work', 'assign_work.work_employee_id = sales_track_details.sales_emp_id', 'left');
 	 $this->db->join('area', 'area.a_id = assign_work.allocated_area ', 'left');
+	 $this->db->where('sales_track_details.sales_emp_id',$emp_id);
 	return $this->db->get()->row_array();	
 	
    }
 
-	 public function get_sales_detais_date_wise(){
+	 public function get_sales_detais_date_wise($emp_id){
 	   $this->db->select('sales_track_details.*')->from('sales_track_details');
 	    $this->db->group_by('sales_track_details.work_date');
+		$this->db->where('sales_track_details.sales_emp_id',$emp_id);
+		 return $this->db->get()->result_array();
+	}
+	
+	public function get_employee_previous_date_location_details($emp_id,$date){
+		 $this->db->select('sales_track_details.*')->from('sales_track_details');
+		$this->db->where('sales_track_details.sales_emp_id',$emp_id);
+		$this->db->where('sales_track_details.work_date',$date);
+		 return $this->db->get()->result_array();
+	}
+	public function get_employee_current_date_location_details($emp_id,$date){
+		 $this->db->select('sales_track_details.*')->from('sales_track_details');
+		$this->db->where('sales_track_details.sales_emp_id',$emp_id);
+		$this->db->where('sales_track_details.work_date',$date);
 		 return $this->db->get()->result_array();
 	}
 	   
