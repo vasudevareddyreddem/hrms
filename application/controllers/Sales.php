a27a44a73961825a726c2010729fa7c53465f570
vasudevareddyreddem <vasudevareddy549@gmail.com>
2018-11-14 11:22:45 +0530

    sales  track  detail  chages  purpose
    sales  track  detail  chages  purpose

--- application/controllers/Sales.php
+++ application/controllers/Sales.php
@@ -36,7 +36,7 @@ class Sales extends In_frontend {
          $admindetails=$this->session->userdata('hrmsdetails');	
 		 $post=$this->input->post();
 		 $emp_id=base64_decode($this->uri->segment(3));
-		 $data['sale_man_details']=$this->Sales_model->get_sales_man_details($emp_id);
+		 
 		
 		 if(isset($post['work_date']) && $post['work_date']!=''){
 				$dat=explode('/',$post['work_date']);
@@ -46,6 +46,26 @@ class Sales extends In_frontend {
 				$data['prevoiu_date']=$previous_date;
 				$data['prevoius_date']=$this->Sales_model->get_employee_previous_date_location_details($emp_id,$previous_date);
 				$data['current_date']=$this->Sales_model->get_employee_current_date_location_details($emp_id,$current_date);
+				$data['sale_man_details']=$this->Sales_model->get_sales_man_details_with_date($emp_id,$current_date);
+				$current_work_location=$this->Sales_model->get_work_location_name($emp_id,$current_date);
+				 $previous_work_location=$this->Sales_model->get_work_location_name($emp_id,$previous_date);
+				 
+				 if(isset($previous_work_location) && count($previous_work_location)>0){
+					 foreach($previous_work_location as $lis){
+						 $p_locations[]=$lis['area'];
+					}
+				  $data['previous_work_location']=implode( ',', $p_locations );
+				 }else{
+					 $data['previous_work_location']=''; 
+				 }
+				 if(isset($current_work_location) && count($current_work_location)>0){
+					 foreach($current_work_location as $lis){
+						 $c_locations[]=$lis['area'];
+					}
+				  $data['current_work_location']=implode( ',', $c_locations );
+				 }else{
+					 $data['current_work_location']=''; 
+				 }
 				 
 		 }else{
 				 $data['today_date']=date('Y-m-d');
@@ -53,10 +73,28 @@ class Sales extends In_frontend {
 				 
 				 $data['prevoius_date']=$this->Sales_model->get_employee_previous_date_location_details($emp_id,date('Y-m-d', strtotime(' -1 day')));
 				 $data['current_date']=$this->Sales_model->get_employee_current_date_location_details($emp_id,date('Y-m-d'));
-				  
+				 $data['sale_man_details']=$this->Sales_model->get_sales_man_details_with_date($emp_id,date('Y-m-d'));
+				  $current_work_location=$this->Sales_model->get_work_location_name($emp_id,date('Y-m-d'));
+				 $previous_work_location=$this->Sales_model->get_work_location_name($emp_id,date('Y-m-d', strtotime(' -1 day')));
+				 if(isset($previous_work_location) && count($previous_work_location)>0){
+					 foreach($previous_work_location as $lis){
+						 $p_locations[]=$lis['area'];
+					}
+				  $data['previous_work_location']=implode( ',', $p_locations );
+				 }else{
+					 $data['previous_work_location']=''; 
+				 }
+				 if(isset($current_work_location) && count($current_work_location)>0){
+					 foreach($current_work_location as $lis){
+						 $c_locations[]=$lis['area'];
+					}
+				  $data['current_work_location']=implode( ',', $c_locations );
+				 }else{
+					 $data['current_work_location']=''; 
+				 }
 				 
 			}
-		 //echo'<pre>';print_r($data);exit;
+		// echo'<pre>';print_r($data);exit;
 	     $this->load->view('sales/track-details',$data);
 	     $this->load->view('html/footer');   
 		}else{
