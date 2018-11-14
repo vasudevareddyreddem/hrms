1e0ae9a60bac6f46b1a56a64ad0bdfd2f5484c55
vasudevareddyreddem <vasudevareddy549@gmail.com>
2018-11-14 15:35:19 +0530

    sales  exuctive  daily work export
    sales  exuctive  daily work export

--- application/controllers/Export.php
+++ application/controllers/Export.php
@@ -72,8 +72,49 @@ class Export extends In_frontend {
 	    if($this->session->userdata('hrmsdetails'))
 		{
 			 $post=$this->input->post();
-			 $usersData=$this->Export_model->get_sales_man_daily_work_report();
-			 echo '<pre>';print_r($usersData);exit;
+			 if($post['type']=='Daily'){
+				$usersData=$this->Export_model->get_sales_man_daily_work_report(date('Y-m-d'));
+			 }else if($post['type']=='Weekly'){
+				 $usersData=$this->Export_model->get_sales_man_week_work_report(date('Y-m-d'));
+			 }else if($post['type']=='Monthly'){
+				 $usersData=$this->Export_model->get_sales_man_month_work_report(date('m'));
+			 }
+			 
+			 //echo '<pre>';print_r($usersData);exit;
+			 
+			 $work_details=array();
+			 foreach($usersData as $list){
+				 
+				 
+				  if(isset($list['work']) && count($list['work'])>0){
+					 $work_details='';
+					 foreach($list['work'] as $work_list){
+						 $data_work[]='Date:'.$work_list['work_date'].''.$work_list['time'].'Area:'.$work_list['area_name'].'_'.$work_list['area_location'].', ';
+					 }
+					  $work_details=implode("",$data_work);
+					}else{
+						$work_details='';
+					}
+					$data[$list['e_id']]['date']=$list['date'];
+					$data[$list['e_id']]['e_emplouee_id']=$list['e_emplouee_id'];
+					$data[$list['e_id']]['work_details']=$work_details;
+				 //echo '<pre>';print_r($data);exit;
+			}
+			//echo '<pre>';print_r($usersData);exit;
+			 $filename = 'employee_work_schedule'.date('d').'.csv'; 
+			   header("Content-Description: File Transfer"); 
+			   header("Content-Disposition: attachment; filename=$filename"); 
+			   header("Content-Type: application/csv; ");
+			   // get data 
+			   // file creation 
+			   $file = fopen('php://output', 'w');
+				$header = array("Date","Employee Id","Work"); 
+			   fputcsv($file, $header);
+			   foreach ($data as $key=>$line){ 
+				 fputcsv($file,$line); 
+			   }
+			   fclose($file); 
+			   exit;
   		
         }else{
 			$this->session->set_flashdata('error',"Please login and continue");
